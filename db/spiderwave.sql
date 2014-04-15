bb060332f35fee09d5cd49c28992f05ec5996e91
Charles Coqueret <charles.coqueret@devialet.com>
2014-04-15 19:58:29 +0200

    Updating database structure

--- db/spiderwave.sql
+++ db/spiderwave.sql
@@ -1,21 +1,3 @@
-CREATE TABLE radio( id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT,
-                    logo BLOB,
-                    homepage TEXT,
-                    stream_url_ids TEXT,
-                    genre_ids TEXT,
-                    country_id INTEGER,
-                    city_id INTEGER,
-                    valid INTEGER,
-                    webonly INTEGER );
-
-CREATE TABLE city( id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   name TEXT,
-                   country_id INTEGER );
-                   
-CREATE TABLE country( id INTEGER PRIMARY KEY AUTOINCREMENT,
-                      name TEXT UNIQUE );
-                      
 CREATE TABLE genre( id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT UNIQUE );
 
@@ -23,3 +5,35 @@ CREATE TABLE stream_url( id INTEGER PRIMARY KEY AUTOINCREMENT,
                          url TEXT UNIQUE,
                          bitrate INTEGER,
                          format TEXT );
+
+CREATE TABLE country( id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      name TEXT UNIQUE );
+
+CREATE TABLE city( id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT,
+                   country_id INTEGER,
+                   FOREIGN KEY(country_id) REFERENCES country(id) );
+
+CREATE TABLE radio( id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT,
+                    logo BLOB,
+                    homepage TEXT,
+                    country_id INTEGER,
+                    city_id INTEGER,
+                    valid INTEGER,
+                    webonly INTEGER,
+                    FOREIGN KEY(country_id) REFERENCES country(id),
+                    FOREIGN KEY(city_id) REFERENCES city(id) );
+                                             
+CREATE TABLE joingenre( id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        radio_id INTEGER,
+                        genre_id INTEGER,
+                        FOREIGN KEY(radio_id) REFERENCES radio(id),
+                        FOREIGN KEY(genre_id) REFERENCES genre(id) );
+
+CREATE TABLE joinurl( id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      radio_id INTEGER,
+                      stream_url_id INTEGER,
+                      FOREIGN KEY(radio_id) REFERENCES radio(id),
+                      FOREIGN KEY(stream_url_id) REFERENCES stream_url(id) );
+
