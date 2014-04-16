9db3d1ad4d43d52c56212b91dddb33b1142e5a3b
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-04-16 20:00:15 +0200

    Reformat db_wrapper a bit

--- scrappers/db_wrapper.py
+++ scrappers/db_wrapper.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from BeautifulSoup import BeautifulSoup
 import sqlite3 as lite
-import sys
 import os
 
 CON = None
@@ -13,58 +13,58 @@ def connect():
     CON = lite.connect(os.path.join(db_dir, "spiderwave.db"))
 
 def disconnect():
-    global CON
     CON.commit()
     CON.close()
 
 def insert_country(name):
-    global CON
     cur = CON.cursor()
 
     name = unicode(name).encode('utf-8')
 
-    cur.execute('SELECT * FROM country WHERE name="{cnt}"'.format(cnt=name))
+    cur.execute('SELECT * FROM country WHERE name="{cn}"'.format(cn=name))
     country = cur.fetchone()
     if country:
         return country[0]
-    cur.execute('INSERT INTO country ( name ) VALUES ("{cnt}")'.format(cnt=name))
+    cur.execute('INSERT INTO country ( name ) VALUES ("{cn}")'.format(cn=name))
     CON.commit()
 
-    cur.execute('SELECT * FROM country WHERE name="{cnt}"'.format(cnt=name))
+    cur.execute('SELECT * FROM country WHERE name="{cn}"'.format(cn=name))
     return cur.fetchone()[0]
 
 def insert_city(name, country_id):
-    global CON
     cur = CON.cursor()
 
     name = unicode(name).encode('utf-8')
 
-    cur.execute('SELECT * FROM city WHERE name="{cn}" and country_id="{cnt}"'.format(cn=name, cnt=country_id))
+    cur.execute('SELECT * FROM city WHERE name="{cn}" and \
+                country_id="{cnt}"'.format(cn=name, cnt=country_id))
     city = cur.fetchone()
     if city:
         return city[0]
-    cur.execute('INSERT INTO city ( name, country_id )VALUES ("{cn}", {cnt})'.format(cn=name, cnt=country_id))
+    cur.execute('INSERT INTO city ( name, country_id )VALUES ("{cn}", {cnt})'\
+                .format(cn=name, cnt=country_id))
     CON.commit()
     # Then grab the ID and return that.
-    cur.execute('SELECT * FROM city WHERE name="{cn}" and country_id="{cnt}"'.format(cn=name, cnt=country_id))
+    cur.execute('SELECT * FROM city WHERE name="{cn}" and country_id="{cnt}"'\
+                .format(cn=name, cnt=country_id))
     return cur.fetchone()[0]
 
-def insert_streamurl(url, bitrate=0, format=0, valid=0):
-    global CON
+def insert_streamurl(url, bitrate=0, codec=0, valid=0):
     cur = CON.cursor()
 
     cur.execute('SELECT * FROM stream_url WHERE url="{u}"'.format(u=url))
     link = cur.fetchone()
     if link:
         return link[0]
-    cur.execute('INSERT INTO stream_url ( url, bitrate, format, valid ) VALUES ("{u}", {b}, "{f}", {v})'.format(u=url, b=bitrate, f=format, v=valid))
+    cur.execute('INSERT INTO stream_url ( url, bitrate, format, valid ) VALUES\
+            ("{u}", {b}, "{f}", {v})'\
+            .format(u=url, b=bitrate, f=codec, v=valid))
     CON.commit()
     # Then grab the ID and return that.
     cur.execute('SELECT * FROM stream_url WHERE url="{u}"'.format(u=url))
     return cur.fetchone()[0]
 
 def insert_genre(name):
-    global CON
     cur = CON.cursor()
 
     name = unicode(name).encode('utf-8')
@@ -80,78 +80,88 @@ def insert_genre(name):
     return cur.fetchone()[0]
 
 def insert_joingenre(radio_id, genre_id):
-    global CON
     cur = CON.cursor()
 
-    cur.execute('SELECT * FROM joingenre WHERE radio_id="{ri}" and genre_id="{gi}"'.format(ri=radio_id, gi=genre_id))
+    cur.execute('SELECT * FROM joingenre WHERE radio_id="{ri}" and \
+            genre_id="{gi}"'.format(ri=radio_id, gi=genre_id))
     genre = cur.fetchone()
     if genre:
         return genre[0]
-    cur.execute('INSERT INTO joingenre ( radio_id, genre_id ) VALUES ("{ri}", "{gi}")'.format(ri=radio_id, gi=genre_id))
+    cur.execute('INSERT INTO joingenre ( radio_id, genre_id ) VALUES ("{ri}",\
+            "{gi}")'.format(ri=radio_id, gi=genre_id))
     CON.commit()
     # Then grab the ID and return that.
-    cur.execute('SELECT * FROM joingenre WHERE radio_id="{ri}" and genre_id="{gi}"'.format(ri=radio_id, gi=genre_id))
+    cur.execute('SELECT * FROM joingenre WHERE radio_id="{ri}" and \
+            genre_id="{gi}"'.format(ri=radio_id, gi=genre_id))
     return cur.fetchone()[0]
 
 def insert_joinstreamurl(radio_id, stream_url_id):
-    global CON
     cur = CON.cursor()
 
-    cur.execute('SELECT * FROM joinstreamurl WHERE radio_id="{ri}" and stream_url_id="{sui}"'.format(ri=radio_id, sui=stream_url_id))
+    cur.execute('SELECT * FROM joinstreamurl WHERE radio_id="{ri}" and \
+            stream_url_id="{sui}"'.format(ri=radio_id, sui=stream_url_id))
     genre = cur.fetchone()
     if genre:
         return genre[0]
-    cur.execute('INSERT INTO joinstreamurl ( radio_id, stream_url_id ) VALUES ("{ri}", "{sui}")'.format(ri=radio_id, sui=stream_url_id))
+    cur.execute('INSERT INTO joinstreamurl ( radio_id, stream_url_id ) VALUES \
+            ("{ri}", "{sui}")'.format(ri=radio_id, sui=stream_url_id))
     CON.commit()
     # Then grab the ID and return that.
-    cur.execute('SELECT * FROM joinstreamurl WHERE radio_id="{ri}" and stream_url_id="{sui}"'.format(ri=radio_id, sui=stream_url_id))
+    cur.execute('SELECT * FROM joinstreamurl WHERE radio_id="{ri}" and \
+            stream_url_id="{sui}"'.format(ri=radio_id, sui=stream_url_id))
     return cur.fetchone()[0]
 
-def insert_radio( name, city_id, homepage, logo, web_only):
-    global CON
+def insert_radio(name, city_id, homepage, logo, web_only):
     cur = CON.cursor()
 
-    cur.execute('SELECT * FROM radio WHERE name="{n}" and city_id="{ci}"'.format(n=name, ci=city_id))
+    cur.execute('SELECT * FROM radio WHERE name="{n}" and \
+            city_id="{ci}"'.format(n=name, ci=city_id))
     genre = cur.fetchone()
     if genre:
         return genre[0]
-    cur.execute('INSERT INTO radio ( name, logo, homepage, city_id, webonly ) VALUES ("{n}", "{l}", "{h}", "{ci}", "{w}")'.format(n=name, l=logo, h=homepage, ci=city_id, w=web_only))
+    cur.execute('INSERT INTO radio ( name, logo, homepage, city_id, webonly ) \
+            VALUES ("{n}", "{l}", "{h}", "{ci}", "{w}")'.format(n=name, l=logo,
+                h=homepage, ci=city_id, w=web_only))
     CON.commit()
     # Then grab the ID and return that.
-    cur.execute('SELECT * FROM radio WHERE name="{n}" and city_id="{ci}"'.format(n=name, ci=city_id))
+    cur.execute('SELECT * FROM radio WHERE name="{n}" and \
+            city_id="{ci}"'.format(n=name, ci=city_id))
     return cur.fetchone()[0]
 
-def add_radio( name, city, country, stream_urls, genres, logo=None, homepage=None, web_only=0 ):
-    global CON
-    cur = CON.cursor()
+def add_radio(name, city, country, stream_urls, genres, logo=None,
+        homepage=None, web_only=0):
+
+    # Sanitize everything
+    name = BeautifulSoup(unicode(name.strip()).encode('utf-8'), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
+    city = BeautifulSoup(unicode(city.strip()).encode('utf-8'), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
+    country = BeautifulSoup(unicode(country.strip()).encode('utf-8'), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
 
     #Get country id
-    country_id = insert_country( country );
-    
-    #Get city id
-    city_id = insert_city(city, country_id);
+    country_id = insert_country(country)
 
-    stream_url_ids=[]
+    #Get city id
+    city_id = insert_city(city, country_id)
+
+    stream_url_ids = []
     #Get stream_url ids
-    for stream_url in stream_urls:
-        stream_url_ids.append( insert_streamurl(stream_url, 0, 0, 0) )
-    
-    genre_ids=[]
-    #Get genres ids    
+    for stream_url, br in stream_urls:
+        stream_url_ids.append(insert_streamurl(stream_url, bitrate=br))
+
+    genre_ids = []
+    #Get genres ids
     for genre in genres:
-        genre_ids.append( insert_genre(genre) )
+        genre_ids.append(insert_genre(genre))
 
     #Get radio id
-    radio_id = insert_radio( name,  None, city_id, homepage, web_only );
+    radio_id = insert_radio(name, city_id, homepage, logo, web_only)
 
     #update joingenre
     for genre_id in genre_ids:
-        insert_joingenre( radio_id, genre_id )
-    
+        insert_joingenre(radio_id, genre_id)
+
     #update joinstream
     for stream_url_id in stream_url_ids:
-        insert_joinstreamurl( radio_id, stream_url_id )
-    
-    CON.commit()
+        insert_joinstreamurl(radio_id, stream_url_id)
+
     return
 
