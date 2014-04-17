38a9bd989f65196ab08d003ca65468523081c458
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-04-17 15:33:38 +0200

    db_wrapper: Add a sanitize function

--- scrappers/db_wrapper.py
+++ scrappers/db_wrapper.py
@@ -16,6 +16,14 @@ def disconnect():
     CON.commit()
     CON.close()
 
+def sanitize(input_str):
+    output = BeautifulSoup(input_str.strip(), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
+    try:
+        output = unicode(output).encode('utf-8')
+    except UnicodeDecodeError:
+        output = unicode(output_enc.decode('utf-8')).encode('utf-8')
+    return output
+
 def insert_country(name):
     cur = CON.cursor()
 
@@ -63,11 +71,7 @@ def insert_streamurl(url, bitrate=0, codec=0, valid=0):
 def insert_genre(name):
     cur = CON.cursor()
 
-    name = BeautifulSoup(unicode(name.strip()).encode('utf-8'), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
-    try:
-        name = unicode(name).encode('utf-8')
-    except UnicodeDecodeError:
-        name = unicode(name.decode('utf-8')).encode('utf-8')
+    name = sanitize(name)
 
     cur.execute('SELECT * FROM genre WHERE name="{g}"'.format(g=name))
     genre = cur.fetchone()
@@ -132,23 +136,9 @@ def add_radio(name, city, country, stream_urls, genres, logo=None,
         homepage=None, web_only=0):
 
     # Sanitize everything
-    name = BeautifulSoup(name.strip(), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
-    try:
-        name = unicode(name).encode('utf-8')
-    except UnicodeDecodeError:
-        name = unicode(name.decode('utf-8')).encode('utf-8')
-
-    city = BeautifulSoup(city.strip(), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
-    try:
-        city = unicode(city).encode('utf-8')
-    except UnicodeDecodeError:
-        city = unicode(city.decode('utf-8')).encode('utf-8')
-
-    country = BeautifulSoup(country.strip(), convertEntities=BeautifulSoup.HTML_ENTITIES).contents[0]
-    try:
-        country = unicode(country).encode('utf-8')
-    except UnicodeDecodeError:
-        city = unicode(country.decode('utf-8')).encode('utf-8')
+    name = sanitize(name)
+    city = sanitize(city)
+    country = sanitize(country)
 
     #Get country id
     country_id = insert_country(country)
@@ -159,6 +149,7 @@ def add_radio(name, city, country, stream_urls, genres, logo=None,
     stream_url_ids = []
     #Get stream_url ids
     for stream_url, br in stream_urls:
+        stream_url = sanitize(stream_url)
         stream_url_ids.append(insert_streamurl(stream_url, bitrate=br))
 
     genre_ids = []
